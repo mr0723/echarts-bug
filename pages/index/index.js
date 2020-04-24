7963b362ee411db2051bdf9c267112484a205749
unknown <wmr200888@qq.com>
2020-04-24 17:02:42 +0800

    echats图的堆积bug

---
+++ pages/index/index.js
@@ -0,0 +1,46 @@
+const app = getApp();
+
+Page({
+  onShareAppMessage: function (res) {
+    return {
+      title: 'ECharts 可以在微信小程序中使用啦！',
+      path: '/pages/index/index',
+      success: function () {},
+      fail: function () {}
+    }
+  },
+  data: {
+    charts: [{
+      id: 'line',
+      name: '折线图'
+    }, {
+      id: 'funnel',
+      name: '漏斗图'
+    }, {
+      id: 'gauge',
+      name: '仪表盘'
+    }],
+    chartsWithoutImg: [{
+      id: 'lazyLoad',
+      name: '延迟加载图表'
+    }, {
+      id: 'multiCharts',
+      name: '一个页面中多个图表'
+    }, {
+      id: 'move',
+      name: '页面不阻塞滚动'
+    }, {
+      id: 'saveCanvas',
+      name: '保存 Canvas 到本地文件'
+    }]
+  },
+
+  onReady() {
+  },
+
+  open: function (e) {
+    wx.navigateTo({
+      url: '../' + e.target.dataset.chart.id + '/index'
+    });
+  }
+});
